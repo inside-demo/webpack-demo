ad87f065acdc9e54e197fca342250f6a777af0b0
Ivan Demidov <demidov@220-volt.ru>
2015-12-17 12:11:57 +0300

    issue-1 resolved path ava

--- test/test-Mymodule.js
+++ test/test-Mymodule.js
@@ -1,6 +1,6 @@
 /* eslint linebreak-style: [2, "windows"] */
 import test from 'ava';
-import '../node_modules/ava/node_modules/babel-core/register';
+import 'babel-core/register';
 import Plugin from '../src/mymodule.js';
 
 test('test my module', t => {
