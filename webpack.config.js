f192dbb90c64ea4b952922ead18708fb0b6afcbf
Ivan Demidov <demidov@220-volt.ru>
2016-02-26 12:57:59 +0300

    Fixed xo report error

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  module: {
-  	loaders: [
-  		{
-  			test: /\.css$/,
-  			loader: 'style-loader!css-loader'
-  		},
-  		{
-  			test: /\.js[x]?$/,
-  			loader: 'babel'
-  		},{
+	module: {
+		loaders: [{
+			test: /\.css$/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.js[x]?$/,
+			loader: 'babel'
+		}, {
 			test: /\.jpg$/,
 			loader: 'file-loader'
-		}
-  	]
-  }
-}
+		}]
+	}
+};
